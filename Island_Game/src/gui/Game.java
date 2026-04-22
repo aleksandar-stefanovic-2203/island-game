43e463073c831d8d31d9ad27b197d30a7311aac8
aleksandar-stefanovic-2203 <aleksandar.stefanovic2203@gmail.com>
2026-04-23 00:48:59 +0200

    Updated map loading

--- Island_Game/src/gui/Game.java
+++ Island_Game/src/gui/Game.java
@@ -10,6 +10,7 @@ import java.util.ArrayList;
 public class Game extends Frame {
 		
 	private static int numberOfCells = 30;
+	private static String mapPath = "Map.txt";
 	private Panel mapPanel = new Panel(new GridLayout(30, 30));
 	private Map map = new Map(numberOfCells, numberOfCells);
 	private Button restartButton = new Button("Restart game");
@@ -91,11 +92,16 @@ public class Game extends Frame {
 		message.setText("");
 		URL url;
 		HttpURLConnection con;
-		try {			
-			url = new URL("https://jobfair.nordeus.com/jf24-fullstack-challenge/test");
-			con = (HttpURLConnection) url.openConnection();
-			con.setRequestMethod("GET");
-			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+		try {	
+			/*
+			 * This part was used while the challenge was still active.
+			 * Since the link has expired, loading a map is done from a file Map.txt
+			 */			
+//			url = new URL("https://jobfair.nordeus.com/jf24-fullstack-challenge/test");
+//			con = (HttpURLConnection) url.openConnection();
+//			con.setRequestMethod("GET");
+//			BufferedReader in = new BufferedReader(new InputStreamReader(con.getInputStream()));
+			BufferedReader in = new BufferedReader(new InputStreamReader(new FileInputStream(new File(mapPath))));
 			String inputLine;
 			String words[] = new String[numberOfCells];
 			int i = 0;
@@ -104,7 +110,7 @@ public class Game extends Frame {
 				for(int j = 0; j < numberOfCells; j++) {
 					int height = Integer.parseInt(words[j]);
 					if(height == 0) map.cells[i][j] = new Water();
-					else map.cells[i][j] = new Land(height);				
+					else map.cells[i][j] = new Land(height);
 				}
 				
 				i++;
